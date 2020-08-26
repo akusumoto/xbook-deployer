24f4f328d37520e4abca17d8c4e127c862874417
Akira Kusumoto <akira.kusumoto@hennge.com>
2020-08-26 17:58:08 +0900

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -38,104 +38,24 @@ fn load_xbooks(dir: &PathBuf) -> Result<Vec<PathBuf>, io::Error> {
         }
     }
     Ok(files)
-
-    /*
-    for p in fs::read_dir(dir)? {
-        if let Ok(r) = p {
-            let path = r.path();
-            if path.is_file() {
-                if let Some(ext) = path.extension() {
-                    if let Ok(s) = ext.to_os_string().into_string() {
-                        if s == "zip" {
-                            files.push(path.to_path_buf());
-                        }
-                    }
-                }
-            }
-        }
-    }
-    Ok(files)
-    */
-
-    /*
-    fs::read_dir(dir).and_then(|readdir| {
-        let mut files: Vec<PathBuf> = Vec::new();
-        for p in readdir {
-            let path = p.unwrap().path();
-            if path.is_file() {
-                path.extension().and_then(|ext| {
-                    match ext.to_os_string().into_string() {
-                        Ok(s) => {
-                            if s == "zip" {
-                                files.push(path.to_path_buf());
-                            }
-                            Some(())
-                        },
-                        Err(_) => None,
-                    }
-                });
-            }
-        }
-        Ok(files)
-    })
-    */
-
-    /*
-    match fs::read_dir(dir) {
-        Ok(readdir) => {
-            let mut files: Vec<PathBuf> = Vec::new();
-            //for p in fs::read_dir(dir).unwrap() {
-            for p in readdir {
-                let path = p.unwrap().path();
-                if path.is_file() {
-                    match path.extension() {
-                        Some(ext) => {
-                            match ext.to_os_string().into_string() {
-                                Ok(s) => {
-                                    if s == "zip" {
-                                        files.push(path.to_path_buf());
-                                    }
-                                },
-                                Err(_) => (),
-                            }
-                        },
-                        None => (),
-                    }
-                }
-            }
-            Ok(files)
-        },
-        Err(e) => Err(e.to_string())
-    }
-    */
 }
 
 fn move_file(src: &PathBuf, dst: &PathBuf) -> Result<u64, fs_extra::error::Error>{
-    //println!("{}", src.file_name().into_os_string().into_strong().unwrap());
-
     let opt = file::CopyOptions::new();
     Ok(file::move_file(src, dst, &opt)?)
 }
 
 fn main() -> Result<(), String>{
     let (src, dst) = load_args()?;
-    /*
-    let (src, dst) = match load_args() {
-        Ok((s, d)) => (s, d),
-        Err(e) => { 
-            println!("{}", e);
-            return
-        },
-    };
-    */
-    //println!("{} -> {}", src.display(), dst.display());
 
     for src_file in load_xbooks(&src).unwrap() {
         let mut dst_file = PathBuf::new();
         dst_file.push(&dst);
         dst_file.push(src_file.file_name().unwrap());
-        println!("{} -> {}", src_file.display(), dst_file.display());
-        let _ = move_file(&src_file, &dst_file);
+        match move_file(&src_file, &dst_file) {
+            Ok(_) => println!("{} -> {}", src_file.display(), dst_file.display()),
+            Err(e) => println!("{}", e.to_string()),
+        }
     }
 
     Ok(())
